aed6d5f75716debd4edba60fc3283a3b65a1d070
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-12 19:15:49 -0300

    add get_pokemon_info method to PokemonAPI class in api/pokemon.py file

--- api/pokemon.py
+++ api/pokemon.py
@@ -6,3 +6,17 @@ class PokemonAPI:
 
         self.name = name
         self.base_url = "https://pokeapi.co/api/v2/"
+
+    def get_pokemon_info(self):
+
+        url = f"{self.base_url}/pokemon/{self.name}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            pokemon_data = response.json()
+            return pokemon_data
+
+        else:
+
+            print(f"Failed to retrieve data {response.status_code}")
