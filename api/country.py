7368d0b4bc71d07c5c469d78c4ef920990b5930c
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-12 18:45:42 -0300

    add show_country_info method to CountryAPI class in api/country.py file

--- api/country.py
+++ api/country.py
@@ -5,3 +5,28 @@ class CountryAPI:
     def __init__(self, country):
 
         self.url = f"https://restcountries.com/v3.1/name/{country}"
+
+    def show_country_info(self):
+
+        response = requests.get(self.url)
+
+        if response.status_code == 200:
+
+            data = response.json()
+
+            country_data = data[0]
+
+            currencies = country_data["currencies"]
+            currency_code = list(currencies.keys())[0]
+            currency_name = currencies[currency_code]["name"]
+            currency_symbol = currencies[currency_code]["symbol"]
+            languages = country_data["languages"]
+
+            return [
+
+                f"Capital: {country_data["capital"][0]}",
+                f"Population: {country_data["population"]}",
+                f"Currency: {currency_name} ({currency_symbol})",
+                f"LANGUAGES:"
+
+            ]
