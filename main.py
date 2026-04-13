cc2817b985198347cd0ced71c27fc5a6f7fa7d7a
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-13 09:49:30 -0300

    create main game loop function and add pokemon api in "main.py"

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ print("======== WELCOME TO THE API HUB ===========")
 # ----------------------------------------
 
 # ------------ MAIN GAME LOOP ------------
+def main():
+
+    while True:
+
+        menu()
+
+        selection = input("\n>> ")
+
+        if selection == "1":
+
+            print("\nEnter a pokemon name to view stats and information!")
+
+            try:
+
+                name = input("\n>> ")
+
+                pokemon_search = PokemonAPI(name)
+
+                pokemon = pokemon_search.get_pokemon_info()
+
+                show_pokemon_info = pokemon_search.show_pokemon_info()
+
+                print(show_pokemon_info)
+
+                time.sleep(1.5)
+
+            except:
+
+                print("\nSomething went wrong... is the pokemon name valid? Try again!")
+
+                time.sleep(1.5)
