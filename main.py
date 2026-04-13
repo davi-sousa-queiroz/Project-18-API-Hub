fb8aeebed08bed8e48e95f137e285b1ec71d1106
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-13 09:23:29 -0300

    import all required custom modules

---
+++ main.py
@@ -0,0 +1,8 @@
+# ------------ UTILITY FILES -------------
+from utils.file_handler import FileHandler
+from utils.menu import menu
+# ---------- API PYTHON FILES ------------
+from api.country import CountryAPI
+from api.joke import JokeAPI
+from api.pokemon import PokemonAPI
+# ----------------------------------------
