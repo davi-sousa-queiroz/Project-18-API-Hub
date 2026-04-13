c831a50bce9bffd9ea29c3c083dd154fed3ddfd8
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-13 09:17:01 -0300

    complete menu.py and file_handler.py files in utils directory

--- utils/menu.py
+++ utils/menu.py
@@ -1,9 +1,13 @@
-print("\n========= MENU =========")
-print("1. Pokemon API")
-print("2. Country API")
-print("3. Joke API")
-print("4. View Request History")
-print("5. Quit")
-print("========================")
+def menu():
 
-print("\nSelect an API to utilize")
+    print("\n========= MENU =========")
+    print("1. Pokemon API")
+    print("2. Country API")
+    print("3. Joke API")
+    print("4. View Request History")
+    print("5. Quit")
+    print("========================")
+
+    print("\nSelect an API to utilize")
+
+#
