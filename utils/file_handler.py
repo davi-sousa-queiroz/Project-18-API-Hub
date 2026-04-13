5ea0e4d2139010e40b03cd47885ef9238cc99423
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-13 09:13:50 -0300

    add load method to FileHandler class in utils/file_handling.py

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -5,3 +5,23 @@ class FileHandler:
     def __init__(self, file_name="data.json"):
 
         self.file_name = file_name
+
+    def load(self):
+
+        try:
+
+            with open(self.file_name, "r") as file:
+                return json.load(file)
+
+        except FileNotFoundError:
+
+            data = {
+
+                "pokemon": [],
+                "countries": [],
+                "joke_count": 0
+
+            }
+
+            self.save(data)
+            return data
