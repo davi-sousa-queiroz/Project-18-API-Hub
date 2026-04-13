64ce3ec852f834cb6abcd2f1d7684f3e883c655c
Davi Queiroz <daviqueiroz26@proton.me>
2026-04-13 09:06:32 -0300

    create menu format in utils/menu.py

---
+++ utils/menu.py
@@ -0,0 +1,9 @@
+print("\n========= MENU =========")
+print("1. Pokemon API")
+print("2. Country API")
+print("3. Joke API")
+print("4. View Request History")
+print("5. Quit")
+print("========================")
+
+print("\nSelect an API to utilize")
